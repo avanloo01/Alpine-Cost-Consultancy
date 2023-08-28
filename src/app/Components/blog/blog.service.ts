5c0ae2f9fd4046c673a4e0103dff42a572cf2524
Arthur Van Loo <arthur@vanloo.biz>
2023-08-28 11:18:21 +0200

    Update blog.service.ts

--- src/app/Components/blog/blog.service.ts
+++ src/app/Components/blog/blog.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class BlogService {
   private blogPosts = [
-    { id: 1, title: 'Welcome', content: '<h2>Welcome!<h2/> On this blog, you will find analyses of firms\' cost structures and references of our work.', date: '27/08/2023' },
+    { id: 1, title: 'Welcome', content: 'On this blog, you will find analyses of firms\' cost structures and references of our work.', date: '27/08/2023' },
     // { id: 2, title: 'Second Post', content: 'This is the content of the second post.', date: '20/12/2023' },
     // { id: 3, title: 'Second Post', content: 'This is the content of the second post.', date: '20/12/2023' },
     // { id: 4, title: 'Second Post', content: 'This is the content of the second post.', date: '20/12/2023' },
